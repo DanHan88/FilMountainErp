7f58f5f4ad6832b11601bf42ffd5ef733b859a6d
ZetaCube <ZetaCube@DESKTOP-GSV6L5Q>
2024-01-11 14:42:43 +0900

    [Product page 구성]

--- FilMountainErp/src/main/java/zetacube/controller/LoginController.java
+++ FilMountainErp/src/main/java/zetacube/controller/LoginController.java
@@ -12,23 +12,29 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/user")
 public class LoginController {
 
-	@GetMapping(value={"/"})
+	@GetMapping(value={""})
     public ModelAndView login(HttpServletRequest request) {
         ModelAndView mav = new ModelAndView();
-        mav.setViewName("views/authentication/split/sign-in");
+        mav.setViewName("views/user/login/sign-in");
         return mav;
     }
-    @GetMapping(value={"/index"})
+    @GetMapping(value={"/dashboard"})
     public ModelAndView index(HttpServletRequest request) {
         ModelAndView mav = new ModelAndView();
-        mav.setViewName("views/index");
+        mav.setViewName("views/user/dashboard");
         return mav;
     }
     @GetMapping(value={"/notifications"})
     public ModelAndView notifications(HttpServletRequest request) {
         ModelAndView mav = new ModelAndView();
-        mav.setViewName("views/notifications");
+        mav.setViewName("views/user/notifications");
         return mav;
     }
+   /* @GetMapping(value={"/product"})
+    public ModelAndView product(HttpServletRequest request) {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("views/user/product");
+        return mav;
+    }*/
 	
 }
